9102fdc8290739e8f408c1de394e4783813ec22f
omidkarami77 <omidtkd77@gmail.com>
2020-04-27 14:45:47 +0430

    Crime Repository CRUD method

--- app/src/main/java/com/example/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/example/criminalintent/CrimeActivity.java
@@ -1,6 +1,8 @@
 package com.example.criminalintent;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
@@ -10,6 +12,15 @@ public class CrimeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+        if (fragment == null) {
+            fragmentManager
+                    .beginTransaction()
+                    .add(R.id.fragment_container, new CrimeFragment())
+                    .commit();
+        }
     }
 }
 
---
+++ app/src/main/java/com/example/criminalintent/model/CrimeRepository.java
@@ -0,0 +1,67 @@
+package com.example.criminalintent.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class CrimeRepository {
+    public static CrimeRepository sCrimeRepository;
+
+    List<Crime> mCrimes;
+
+    public static CrimeRepository getInstance() {
+        if (sCrimeRepository == null) {
+            sCrimeRepository = new CrimeRepository();
+        }
+        return sCrimeRepository;
+    }
+
+    private CrimeRepository() {
+        mCrimes = new ArrayList<>();
+        for (int i = 0; i <= 100; i++) {
+            Crime crime = new Crime();
+            crime.setTitle("Crime " + i);
+            crime.setSolved(i % 2 == 0);
+            mCrimes.add(crime);
+        }
+    }
+
+    public void inserCrime(Crime crime) {
+        mCrimes.add(crime);
+    }
+
+    public void updateCrime(Crime crime) throws Exception {
+        Crime c = getCrime(crime.getId());
+        if (c == null) {
+            throw new Exception("this crime does not exist");
+        }
+        c.setTitle(crime.getTitle());
+        c.setDate(crime.getDate());
+        c.setSolved(crime.isSolved());
+    }
+
+    public void deleteCrime(Crime crime) throws Exception {
+        Crime c = getCrime(crime.getId());
+        if (c == null) {
+            throw new Exception("this crime does not exist");
+        }
+        mCrimes.remove(c);
+    }
+
+    public Crime getCrime(UUID uuid) {
+        for (Crime crime : mCrimes) {
+            if (crime.getId().equals(uuid)) {
+                return crime;
+            }
+        }
+        return null;
+    }
+
+    public void setCrimes(List<Crime> mCrimes) {
+        this.mCrimes = mCrimes;
+    }
+
+    public List<Crime> getCrimes() {
+        return mCrimes;
+    }
+}
